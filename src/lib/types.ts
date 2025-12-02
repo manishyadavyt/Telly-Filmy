d5c757720c27135d2c93cafd1add9be5a04b015d
Manish Yadav <manishyadavmanish775@gmail.com>
2025-12-02 22:26:43 +0530

    Update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,21 +1,26 @@
 export interface Post {
-  images: never[];
   id: string;
   slug: string;
   title: string;
   excerpt: string;
   content: string;
-  imageUrl: string;
-  imageHint: string;
+
+  imageUrl: string;       // cover photo
+  images?: string[];      // additional photos
+  imageHint?: string;
+
   category: string;
   tags: string[];
+
   author: {
     name: string;
     avatarUrl: string;
   };
+
   date: string;
   isTopStory: boolean;
   isTrending: boolean;
   views: number;
-  videoUrl?: string; // optional video URL for embedding videos
+
+  videoUrl?: string;
 }
